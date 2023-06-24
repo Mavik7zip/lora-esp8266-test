5a04c147197dce35cada52106c343d4d2aaa40ed
Gabriele Vallosio <84291631+Mavik7zip@users.noreply.github.com>
2023-06-25 01:20:37 +0200

    Update main.cpp

--- test lora sender/src/main.cpp
+++ test lora sender/src/main.cpp
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <ArduinoJson.h>
  
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -10,22 +11,20 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET      -1  // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
- 
- 
+
+
 #define ss 15
 #define rst 16
 #define dio0 2
-int counter = 0;
- 
- 
-void setup() 
-{
+
+int counter;
+
+void setup() {
   Serial.begin(9600);
- 
+
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
   }
 
   Serial.println("LoRa Sender");
@@ -42,8 +41,7 @@ void setup()
   LoRa.setSignalBandwidth(62.5E3);
 }
  
-void loop() 
-{
+void loop() {
   Serial.print("Sending packet: ");
   Serial.println(counter);
  
@@ -57,9 +55,9 @@ void loop()
   
   // send packet
   LoRa.beginPacket();
-  LoRa.print(F("Pkt No:"));
+  LoRa.println("ciro");
   LoRa.println(counter);
-  
+  LoRa.println(6);
   LoRa.endPacket();
  
   counter++;
