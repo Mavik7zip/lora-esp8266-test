1bc223d3edafba123ff6671a051d1933017d670d
Gabriele Vallosio <84291631+Mavik7zip@users.noreply.github.com>
2023-06-28 16:33:58 +0200

    Update main.cpp

--- test lora reciver/src/main.cpp
+++ test lora reciver/src/main.cpp
@@ -15,32 +15,44 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define rst 0
 #define dio0 15
 
+
 struct strt_pkt{
   String text;
-  String no;
-  String numero;
+  int no;
+  int dev_ping;
   int rssi;
   int snr;
 };
 
+void print_display(strt_pkt packet){
+  display.clearDisplay(); //odio i display
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print(F("Rssi: "));
+  display.print(packet.rssi);
+  display.setCursor(0,30);
+  display.print(F("Snr: "));
+  display.print(packet.snr);
+  display.display();
+}
+
+void print_serial(strt_pkt packet){
+  Serial.println("text");
+  Serial.println(packet.text);
+  Serial.println("no pkt");
+  Serial.println(packet.no);
+  // Serial.println("dev ping");
+  // Serial.println(packet.dev_ping);
+  Serial.println("rssi");
+  Serial.println(packet.rssi);
+  Serial.println("snr");
+  Serial.println(packet.snr);
+}
 
 void print_data(strt_pkt packet){
-  Serial.println(packet.text);
-  Serial.println(packet.no);
-  Serial.println(packet.text);
-  Serial.println(packet.rssi);
-  Serial.println(packet.snr);
-
-  // display.clearDisplay(); //odio i display
-  // display.setTextSize(2);
-  // display.setTextColor(WHITE);
-  // display.setCursor(0,0);
-  // display.print(F("Rssi: "));
-  // display.print(packet.rssi);
-  // display.setCursor(0,30);
-  // display.print(F("Snr: "));
-  // display.print(packet.snr);
-  // display.display();
+  print_serial(packet);
+  print_display(packet);
 }
 
 void read_packet(int packet_size){
@@ -48,33 +60,38 @@ void read_packet(int packet_size){
   String tmp;
   char tmp_char;
   int j = 0;
+  int timestamp = millis(); // + base_time;
 
+  
   for (int i = 0; i < packet_size; i++){
     tmp_char = (char)LoRa.read();
+
     if(tmp_char != '\n'){
       tmp += tmp_char;
     }else{
-      j++;
-      switch (j)
-      {
-      case 1:
+
+      switch (j){
+      case 0:
         packet.text = tmp;
         break;
 
-      case 2:
-        packet.no = tmp;
+      case 1:
+        packet.no = tmp.toInt();
         break;
 
-      case 3:
-        packet.numero = tmp;
+      case 2:
+        packet.dev_ping = tmp.toInt();
         break;
       }
+
+      j++;
       tmp = "";
     }
   }
 
   packet.rssi = LoRa.packetRssi();
   packet.snr = LoRa.packetSnr();
+  // packet.dev_ping = timestamp - packet.dev_ping;
 
   print_data(packet);
 }
@@ -100,9 +117,10 @@ void setup() {
     Serial.println("Starting LoRa");
   }
 
-  LoRa.setTxPower(20);
+  LoRa.setTxPower(18);
   LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(62.5E3);
+  LoRa.setSignalBandwidth(7.8E3);
+  LoRa.setCodingRate4(8);
 
   LoRa.onReceive(read_packet);
   LoRa.receive();
